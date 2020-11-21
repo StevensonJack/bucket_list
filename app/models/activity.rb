f35de6970c5a9e4e623b334ec684331a6f048f10
Jack Stevenson <jack.stevenson@allocator.com>
2020-11-21 16:07:15 +0000

    connected skyscanner to offers

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,4 +2,28 @@ class Activity < ApplicationRecord
   belongs_to :bucket
   has_many :offers, dependent: :destroy
   validates :title, :budget, :location, :time_frame, :people_number, :category, presence: true
+
+  after_update :skyscanner_api
+
+  private
+
+  def skyscanner_api
+    self.offers.delete_all
+
+    country = "United Kingdom"
+    currency = "GBP"
+    originplace = "London"
+    # destinationplace = "Paris"
+    destinationplace = self.location
+    leaving_date = "2020-11-25"
+    return_date = "2020-11-27"
+
+    results = Skyscanner.new(country, currency, originplace, destinationplace, leaving_date, return_date).search_flights
+
+    results.each do |quote|
+      offer = Offer.new(rating: 0, price: quote["MinPrice"], start_date: leaving_date, end_date: return_date)
+      offer.activity = self
+      offer.save
+    end
+  end
 end
