f52c9810d01a9b39cf8333d0dbf4b6288062fa5b
Jack Stevenson <jack.stevenson@allocator.com>
2020-11-24 20:09:44 +0000

    nick said it was fine

--- app/lib/skyscanner_test.rb
+++ app/lib/skyscanner_test.rb
@@ -1,26 +1,26 @@
-require 'uri'
-require 'net/http'
-require 'openssl'
-# require 'json'
-require 'byebug'
-require 'unirest'
-require_relative 'skyscanner.rb'
+# require 'uri'
+# require 'net/http'
+# require 'openssl'
+# # require 'json'
+# require 'byebug'
+# require 'unirest'
+# require_relative 'skyscanner.rb'
 
 
-country = "United Kingdom"
-currency = "GBP"
-originplace = "London"
-destinationplace = "Shanghai"
-outboundpartialdate = "2020-11-22"
-inboundpartialdate = "2020-11-23"
+# country = "United Kingdom"
+# currency = "GBP"
+# originplace = "London"
+# destinationplace = "Shanghai"
+# outboundpartialdate = "2020-11-22"
+# inboundpartialdate = "2020-11-23"
 
-results = Skyscanner.new(country, currency, originplace, destinationplace, outboundpartialdate, inboundpartialdate).search_flights
+# results = Skyscanner.new(country, currency, originplace, destinationplace, outboundpartialdate, inboundpartialdate).search_flights
 
-# puts results
+# # puts results
 
-results.each do |x|
-  puts x
-end
+# results.each do |x|
+#   puts x
+# end
 
 # get_carrier_by_id method that can get the full object
 
