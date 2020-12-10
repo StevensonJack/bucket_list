e27c0abe107ff4917d83d08787da13fe4c05cd0c
Jack Stevenson <jack.stevenson@allocator.com>
2020-12-10 21:01:15 +0000

    finished the seed file

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
   def activity_price_color(activity)
-    if activity.rating <= 1
-      return "green"
-    elsif activity.rating > 1 && activity.rating < 1.1
-      return "orange"
-    else
-      return "red"
+  unless activity.rating.nil?
+      if activity.rating <= 1
+        return "green"
+      elsif activity.rating > 1 && activity.rating < 1.1
+        return "orange"
+      else
+        return "red"
+      end
     end
   end
 end
